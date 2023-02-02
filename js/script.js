acd713804879bd6833b5ed345cf4925ce1760ab3
Simone Micheletti <sm.micheletti@gmail.com>
2023-02-02 15:31:30 +0100

    update SM.git.io

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHZDbz3MniCizZp-iXQ8buPyaBLDvVuRU",
+  authDomain: "progetto-stefano-481e3.firebaseapp.com",
+  databaseURL: "https://progetto-stefano-481e3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "progetto-stefano-481e3",
+  storageBucket: "progetto-stefano-481e3.appspot.com",
+  messagingSenderId: "99274032165",
+  appId: "1:99274032165:web:b4ac77bd37f810385c6bcc",
+  measurementId: "G-X2QKKTYSLY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
